db21e9811e475581fa474cd280f7ef92a3417d21
MHAbeysinghe <135607523+MHAbeysinghe@users.noreply.github.com>
2023-06-05 16:21:23 +0530

    Add files via upload

---
+++ Practical 03 - Q5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Input 2 integers : ");
+    scanf("%d %d",&num1,&num2);
+
+    if (num1%num2==0)
+        printf("\n%d is a multiple of %d\n",num1,num2);
+    else
+        printf("\n%d is not a multiple of %d\n",num1,num2);
+}
